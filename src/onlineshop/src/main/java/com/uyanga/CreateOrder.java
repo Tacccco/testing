1170a0f5ab3999974024f63c017c82526660bdaa
Uyanga Mijiddorj <uiangam@yahoo.com>
2021-10-25 16:12:24 +0800

    test

--- src/onlineshop/src/main/java/com/uyanga/CreateOrder.java
+++ src/onlineshop/src/main/java/com/uyanga/CreateOrder.java
@@ -3,27 +3,7 @@ package com.uyanga;
 import org.apache.commons.lang3.Validate;
 
 public class CreateOrder {
-    private final Product product;
-    private double quantity;
 
-    public CreateOrder(Product product, double quantity) {
-        this.product = Validate.notNull(product);
-        if (quantity <= 0)
-        {
-            throw new IllegalArgumentException("Negative quantity is not allowed!") ;
-        }
-        this.quantity = quantity;
-    }
+    
 
-    public Product getProduct() {
-        return product;
-    }
-
-    public double getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(double quantity) {
-        this.quantity = quantity;
-    }
 }
--- src/onlineshop/src/test/java/com/uyanga/CreateOrderTest.java
+++ src/onlineshop/src/test/java/com/uyanga/CreateOrderTest.java
@@ -5,41 +5,6 @@ import org.junit.jupiter.api.Test;
 
 class CreateOrderTest {
 
-    @Test
-    public void ctrThrowsExceptionWhenProductNull()
-    {
-        Assertions.assertThrows(NullPointerException.class, () ->
-                new CreateOrder(null,5.5));
-    }
 
-    @Test
-    public void ctrThrowsExceptionWhenQuantityNegative()
-    {
-        Assertions.assertThrows(IllegalArgumentException.class, () ->
-                new CreateOrder(new Product("pid1", "productName", 55), -11.0));
-    }
-
-    @Test
-    public void ctrThrowsExceptionWhenQuantityEquals0()
-    {
-        Assertions.assertThrows(IllegalArgumentException.class, () ->
-                new CreateOrder(new Product("pid1", "productName", 55), 0.0));
-    }
-
-    @Test
-    public void ctrCreatesInstance()
-    {
-        Product product = new Product("pid1", "productName", 55);
-        CreateOrder createOrder = new CreateOrder(product, 2.0);
-
-        Assertions.assertNotNull(createOrder);
-        Assertions.assertNotNull(createOrder.getProduct());
-        Assertions.assertEquals(product, createOrder.getProduct());
-        Assertions.assertEquals("pid1", createOrder.getProduct().getId());
-        Assertions.assertEquals("productName", createOrder.getProduct().getName());
-        Assertions.assertEquals(55, createOrder.getProduct().getPrice());
-        Assertions.assertEquals(2.0, createOrder.getQuantity());
-
-    }
 
 }
