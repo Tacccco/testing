38fd3f17899c5339f542378736a3e94598aa10ff
Uyanga Mijiddorj <uiangam@yahoo.com>
2021-10-26 16:02:07 +0800

    usecase

--- src/onlineshop/src/main/java/com/uyanga/useCase/CreateOrder.java
+++ src/onlineshop/src/main/java/com/uyanga/useCase/CreateOrder.java
@@ -15,9 +15,10 @@ public class CreateOrder {
     private ProductRepository productRepository;
     private StockRepository stockRepository;
 
-    public CreateOrder(CustomerRepository customerRepository, ProductRepository productRepository) {
+    public CreateOrder(CustomerRepository customerRepository, ProductRepository productRepository, StockRepository stockRepository) {
         this.customerRepository = Validate.notNull(customerRepository);
         this.productRepository = Validate.notNull(productRepository);
+        this.stockRepository = Validate.notNull(stockRepository);
     }
 
     public Order execute(String customerID, List<ProductListItem> orderedProducts)
@@ -30,18 +31,19 @@ public class CreateOrder {
             throw new IllegalArgumentException("customer not found!");
         }
 
-        if(customerRepository.findById(customerID) == null)
-        {
-            throw new IllegalArgumentException("customer not found!");
-        }
-
         double total = 0;
 
         for (ProductListItem p : orderedProducts)
         {
+            if(productRepository.findById(p.getProductID()) == null)
+            {
+                throw new IllegalArgumentException("product not found!");
+            }
+
             double available = new CheckProductAmount(stockRepository, productRepository).execute("sId1",  p.getProductID());
+
             if(available < p.getAmount()){
-                throw new IllegalArgumentException("not available amount of product");
+                throw new IllegalArgumentException("not available this amount of product");
             }
             total += productRepository.findById(p.getProductID()).getPrice() * p.getAmount();
         }
--- src/onlineshop/src/test/java/com/uyanga/useCase/CreateOrderTest.java
+++ src/onlineshop/src/test/java/com/uyanga/useCase/CreateOrderTest.java
@@ -1,11 +1,13 @@
 package com.uyanga.useCase;
 
 import com.uyanga.ProductListItem;
+import com.uyanga.entity.Customer;
 import com.uyanga.entity.Order;
+import com.uyanga.entity.Product;
+import com.uyanga.entity.Stock;
 import com.uyanga.repository.CustomerRepository;
 import com.uyanga.repository.ProductRepository;
 import com.uyanga.repository.StockRepository;
-import com.uyanga.useCase.CreateOrder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -20,12 +22,33 @@ class CreateOrderTest {
     StockRepository stockRepository = Mockito.mock(StockRepository.class);
 
     @Test
+    public void ordThrowsExceptionWhenCustomerRepositoryNull()
+    {
+        Assertions.assertThrows(NullPointerException.class, () ->
+                new CreateOrder(null, productRepository, stockRepository));
+    }
+
+    @Test
+    public void ordThrowsExceptionWhenProductRepositoryNull()
+    {
+        Assertions.assertThrows(NullPointerException.class, () ->
+                new CreateOrder(customerRepository, null, stockRepository));
+    }
+
+    @Test
+    public void ordThrowsExceptionWhenStockRepositoryNull()
+    {
+        Assertions.assertThrows(NullPointerException.class, () ->
+                new CreateOrder(customerRepository, productRepository, null));
+    }
+
+    @Test
     public void ordThrowsExceptionWhenCustomerIdNull()
     {
         Assertions.assertThrows(NullPointerException.class, () ->{
             List<ProductListItem> productList = new ArrayList<>();
             productList.add(new ProductListItem("pid1", 2.0));
-            new CreateOrder(customerRepository, productRepository).execute(null, productList);
+            new CreateOrder(customerRepository, productRepository, stockRepository).execute(null, productList);
         });
     }
 
@@ -35,16 +58,59 @@ class CreateOrderTest {
         Assertions.assertThrows(IllegalArgumentException.class, () ->{
             List<ProductListItem> productList = new ArrayList<>();
             productList.add(new ProductListItem("pid1", 2.0));
-            new CreateOrder(customerRepository, productRepository).execute("", productList);
+            new CreateOrder(customerRepository, productRepository, stockRepository).execute("", productList);
+        });
+    }
+
+    @Test
+    public void ordThrowsExceptionWhenCustomerIdNotValid()
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{
+            Mockito.when(productRepository.findById("pId1")).thenReturn(new Product("pId1", "product_name", 8.8));
+            Mockito.when(customerRepository.findById("cId1")).thenReturn(new Customer("cId1", "customerName", "99999999"));
+            Mockito.when(stockRepository.findById("sId1")).thenReturn(new Stock("sId1", "address_test"));
+
+            List<ProductListItem> productList = new ArrayList<>();
+            productList.add(new ProductListItem("pId1", 2.0));
+            new CreateOrder(customerRepository, productRepository, stockRepository).execute("cId2", productList);
+        });
+    }
+
+    @Test
+    public void ordThrowsExceptionWhenProductIdNotValid()
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{
+            Mockito.when(productRepository.findById("pId1")).thenReturn(new Product("pId1", "product_name", 8.8));
+            Mockito.when(customerRepository.findById("cId1")).thenReturn(new Customer("cId1", "customerName", "99999999"));
+            Mockito.when(stockRepository.findById("sId1")).thenReturn(new Stock("sId1", "address_test"));
+
+            List<ProductListItem> productList = new ArrayList<>();
+            productList.add(new ProductListItem("pId2", 2.0));
+            new CreateOrder(customerRepository, productRepository, stockRepository).execute("cId1", productList);
+        });
+    }
+
+    @Test
+    public void ordThrowsExceptionWhenProductAmountNotValid()
+    {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->{
+            Mockito.when(productRepository.findById("pId1")).thenReturn(new Product("pId1", "product_name", 8.8));
+            Mockito.when(customerRepository.findById("cId1")).thenReturn(new Customer("cId1", "customerName", "99999999"));
+            Mockito.when(stockRepository.findById("sId1")).thenReturn(new Stock("sId1", "address_test"));
+
+            Stock stock = stockRepository.findById("sId1");
+            stock.addToProductList(new ProductListItem("pId1", 1.5));
+            List<ProductListItem> productList = new ArrayList<>();
+            productList.add(new ProductListItem("pId1", 2.0));
+            new CreateOrder(customerRepository, productRepository, stockRepository).execute("cId1", productList);
         });
     }
 
     @Test
     public void ordThrowsExceptionWhenOrderedProductListIdNull()
     {
-        Assertions.assertThrows(NullPointerException.class, () ->{
-            new CreateOrder(customerRepository, productRepository).execute("cID", null);
-        });
+        Assertions.assertThrows(NullPointerException.class, () ->
+                new CreateOrder(customerRepository, productRepository, stockRepository).execute("cID", null));
     }
 
     @Test
@@ -52,24 +118,33 @@ class CreateOrderTest {
     {
         Assertions.assertThrows(IllegalArgumentException.class, () ->{
             List<ProductListItem> productList = new ArrayList<>();
-            new CreateOrder(customerRepository, productRepository).execute("cID", productList);
+            new CreateOrder(customerRepository, productRepository, stockRepository).execute("cID", productList);
         });
     }
 
     @Test
     public void ordCreatesInstance()
     {
+        Mockito.when(productRepository.findById("pId1")).thenReturn(new Product("pId1", "product_name", 8.8));
+        Mockito.when(customerRepository.findById("cId1")).thenReturn(new Customer("cId1", "customerName", "99999999"));
+        Mockito.when(stockRepository.findById("sId1")).thenReturn(new Stock("sId1", "address_test"));
+
+        Stock stock = stockRepository.findById("sId1");
+        stock.addToProductList(new ProductListItem("pId1", 15));
+
         List<ProductListItem> productList = new ArrayList<>();
-        productList.add(new ProductListItem("pid1", 10));
-        Order order = new CreateOrder(customerRepository, productRepository).execute("cus1", productList);
+        productList.add(new ProductListItem("pId1", 2.0));
+
+        Order order =  new CreateOrder(customerRepository, productRepository, stockRepository).execute("cId1", productList);
 
         Assertions.assertNotNull(order);
         Assertions.assertNotNull(order.getId());
 
-        Assertions.assertEquals("cus1", order.getCustomerID());
+        Assertions.assertEquals("cId1", order.getCustomerID());
         Assertions.assertEquals(productList, order.getOrders());
-        Assertions.assertEquals("pid1", order.getOrders().get(0).getProductID());
-        Assertions.assertEquals(10, order.getOrders().get(0).getAmount());
+        Assertions.assertEquals("pId1", order.getOrders().get(0).getProductID());
+        Assertions.assertEquals(2, order.getOrders().get(0).getAmount());
+        Assertions.assertEquals(17.6, order.getTotal());
 
     }
 
